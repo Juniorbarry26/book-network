13e5dafca0681462bd5e7ed6cfab7c7ae78c646d
Alsainey Barry <alsainey7@gmail.com>
2025-03-17 14:34:57 +0000

    Create the role repository for database communication

--- book-network/src/main/java/com/Alsainey/book/role/RoleRepository.java
+++ book-network/src/main/java/com/Alsainey/book/role/RoleRepository.java
@@ -1,2 +1,10 @@
-package com.Alsainey.book.role;public interface RoleRepository {
+package com.Alsainey.book.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String role   );
 }
